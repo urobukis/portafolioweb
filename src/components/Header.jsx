598fd2a106f2b3ee621dc10f5e4c47ab0ad72bc6
Urobukis <adsm120@gmail.com>
2024-01-07 14:02:26 -0300

    correccion css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 const Header=()=>(
 <> 
     <nav className=" sticky top-0 z-50 bg-[#EBEBEB]">
-        <div className="md:mx-8 max-w-full flex flex-wrap items-center justify-between sm:text-center">
+        <div className="md:mx-8 max-w-full md:flex md:flex-wrap md:items-center md:justify-between sm:grid sm:grid-col-1 text-center vmodif">
             <a href="#" className="name">Abraham Silva</a>
-            <ul className="flex flex-cols gap-2">
+            
+            <ul className="flex flex-cols gap-2 sm:grid, grid-cols-1, justify-center">
                 <li><a className="site" href="#home">home</a></li>
                 <li><a className="site" href="#sobremi">sobre mi</a></li>
                 <li><a className="site" href="mailto:silvamillanabraham@gmail.com">contacto</a></li>
